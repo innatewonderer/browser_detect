ce96a1e35f5a4806b50855d1e9899dccd0baa6e2
Faun <faun@riseup.net>
2011-10-06 13:44:54 -0700

    fix deprecation warning for railtie_name method

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,7 +1,6 @@
 # lib/browser_detect/railtie.rb
 module BrowserDetectHelper
 	class Railtie < Rails::Railtie
-	  railtie_name :browser_detect
     initializer "browser_detect.configure_rails_initialization" do
       ApplicationController.send(:include, BrowserDetect)
     end
